3d04428449cd525a4db0d81e797e73b2e9e39f2b
Anarom <forprojectdi@gmail.com>
2018-11-21 20:18:15 +0300

    Renamed

--- PlayerClient.py
+++ PlayerClient.py
@@ -1,37 +1,7 @@
 from Piece import Piece, get_screen_pos
 
 
-class PlayerClient:
-    
-    def __init__(self, window, side):
-        self.side = side
-        self.window = window
-        self.pieces = self.set_pieces()
-        
-
-    def get_turn(self):
-        if isinstance(self, PlayerClient):
-            self.click1 = self.window.root.bind('<Button-1>', self.callback)
-            self.click2 = self.window.root.bind('<Button-3>', self.undo_turn)
-        return self.get_moves()
-           
-    def set_pieces(self):
-        pieces = []
-        offset = {0: 1, 1: 0}
-        if self.side == 'black':
-            start = offset[0]
-            row = 0
-        else:
-            start = offset[1]
-            row = 5
-        for row in range(row, row + 3):
-            column = start
-            for column in range(column, 8, 2):
-                piece = Piece(self.window, self.side, row, column)
-                pieces.append(piece)
-                self.window.field[row][column] = piece
-            start = offset[start]
-        return pieces
+class PlayerClient(Client):
 
     def callback(self, event):
         row, column = self.get_field_pos(event.y, event.x)
@@ -118,15 +88,3 @@ class PlayerClient:
         else:
             piece.set_sprite(f'{self.side}')
         piece.focused = False
-
-    def get_moves(self):
-        has_moves = False
-        can_hit = False
-        for piece in self.pieces:
-            can_hit = piece.find_moves(piece.pos, []) or can_hit
-            if piece.moves:
-                has_moves = True
-            for piece in self.pieces:
-                piece.validate_moves(can_hit) 
-        return has_moves
-
